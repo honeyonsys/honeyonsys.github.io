4febad90ff9306a65b27d65dff92766f3a703bf6
Harish Kumar <honeyonsys@gmail.com>
2018-07-15 22:13:04 +0530

    initial setup tejindra enterprises

---
+++ demo/tejindraenterprises/assets/js/app.js
@@ -0,0 +1,24 @@
+
+/*Main JS for profile page - SM Technologies */
+var app = angular.module('honeyonsys', ['ngRoute']);
+app.config(function($routeProvider){
+    $routeProvider.when('/',{templateUrl: 'views/home.html', controller: 'MainCtrl'})
+    .when('/about', {templateUrl: 'views/about.html', controller: 'MainCtrl'})
+    .when('/services', {templateUrl: 'views/services.html', controller: 'MainCtrl'})
+    .when('/portfolio', {templateUrl: 'views/portfolio.html', controller: 'MainCtrl'})
+    .when('/contact', {templateUrl: 'views/contact.html', controller: 'MainCtrl'})
+    .otherwise({ redirectTo: '/' })
+
+    
+});
+
+
+
+app.controller('MainCtrl', function ($scope, $rootScope, $location) {
+    
+    $scope.isActive = function (viewLocation) {
+        var active = (viewLocation === $location.path());
+        return active;
+   };
+});
+
