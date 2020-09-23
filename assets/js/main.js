0724dc15ce301632f8adee1f4d859f646eaf8d08
honeyonsys <honeyonsys@gmail.com>
2020-09-23 16:50:18 +0530

    added new format for articles json

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,18 +9,24 @@
 
     // fetching the home page blog list
     if ((currentFile.innerHTML == "home")) {
-        var obj = JSON.parse(articles);
-        var blogList = document.getElementById("blogList");
-        obj.forEach(element => {
-            var tags = '';
-            var tagArray = element.keywords.split(",");
-            tagArray.forEach(tag => {
-                tags += '<div class="blogTags">' + tag + '</div>';
+
+        loadJSON(function (response) {
+            // Parse JSON string into object
+            var news_JSON = JSON.parse(response);
+            //console.log(news_JSON["articles"]);
+            var blogList = document.getElementById("blogList");
+            news_JSON["articles"].forEach(element => {
+                var tags = '';
+                var tagArray = element.keywords.split(",");
+                tagArray.forEach(tag => {
+                    tags += '<div class="blogTags">' + tag + '</div>';
+                });
+                blogList.innerHTML += '<div class="blog"><div class="blogTitle"><h3><a href="articles/' + element.url + '">' + element.title + '</a></h3></div><div class="blogBelowLine">' + tags + '<div class="datePublished">published on: <span>' + element.publishedOn + '</span></div></div></div>';
+
             });
-            blogList.innerHTML += '<div class="blog"><div class="blogTitle"><h3><a href="articles/' + element.url + '">' + element.title + '</a></h3></div><div class="blogBelowLine">' + tags + '<div class="datePublished">published on: <span>' + element.publishedOn + '</span></div></div></div>';
-
-
         });
+
+
     }
 
 
@@ -40,3 +46,17 @@
 
 
 })();
+
+function loadJSON(callback) {
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'articles/articleList.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
