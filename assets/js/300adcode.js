1d542d4ab9f57a1be51267cbde504e66c104640d
honeyonsys <honeyonsys@gmail.com>
2022-02-08 23:34:39 +0530

    added 300 ad code on sidebar

---
+++ assets/js/300adcode.js
@@ -0,0 +1,9 @@
+
+atOptions = {
+    'key' : 'dd404529f9bec1c055fce7ac8508aa4d',
+    'format' : 'iframe',
+    'height' : 250,
+    'width' : 300,
+    'params' : {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/dd404529f9bec1c055fce7ac8508aa4d/invoke.js"></scr' + 'ipt>');
